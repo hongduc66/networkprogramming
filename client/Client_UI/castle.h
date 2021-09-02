c853a11034305ec1471aa0e93a59a9f7d0a61c79
LongPhanPip <82559091+LongPhanPip@users.noreply.github.com>
2021-09-02 21:06:33 +0700

    Update castle.h
    Add castle comment

--- client/Client_UI/castle.h
+++ client/Client_UI/castle.h
@@ -4,6 +4,12 @@
 #include "wall.h"
 #include "question.h"
 
+/* Store castle infomation
+    * @param	id              id of the castle
+    * @param	wall	        wall of the castle if exist
+    * @param	occupied_by     team occupy the castle
+    * @param	question        the question of the castle
+*/
 class Castle {
 public:
     int id;
